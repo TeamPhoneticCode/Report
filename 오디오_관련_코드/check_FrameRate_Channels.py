afed2835ca76ab159c5136029c3015a74e407376
201524495 <49744580+201524495@users.noreply.github.com>
2021-05-11 21:13:28 +0900

    채널 + hz 확인

---
+++ 오디오_관련_코드/check_FrameRate_Channels.py
@@ -0,0 +1,13 @@
+OUTPUT_FILE = "noise_04.wav" #file name
+
+# check frame_rate
+from scipy.io.wavfile import read as read_wav
+import os
+os.chdir('./') # path
+sampling_rate, data = read_wav(OUTPUT_FILE)
+print("Frame_Rate : " + str(sampling_rate))
+
+# check channels
+import wave
+f1 = wave.open(OUTPUT_FILE, 'r')
+print("Channels : " + str(f1.getnchannels()))
