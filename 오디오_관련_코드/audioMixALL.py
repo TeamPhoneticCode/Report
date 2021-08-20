5e79aa8943d9b51a4a0453ae599424e5bf64f2b3
201524495 <49744580+201524495@users.noreply.github.com>
2021-08-20 23:49:04 +0900

    Update audioMixALL.py
    audio files mix all path folder

--- 오디오_관련_코드/audioMixALL.py
+++ 오디오_관련_코드/audioMixALL.py
@@ -1,13 +1,6 @@
 import os
-
-path_dir = './data/mini_speech_commands/yes'
-file_list = os.listdir(path_dir)
-##print(file_list)
-file_string = '\n'.join(file_list)
-##print(file_string)
-
-
 from pydub import AudioSegment
+import random
 
 ##import soundfile as sf
 ##from pydub.utils import mediainfo
@@ -15,35 +8,50 @@ from pydub import AudioSegment
 SAMPLING_RATE = 16000
 MONO = 1
 
-number = 46
-Sound = "left"
+Sound_paht_dir = './datas'
+Sound = os.listdir(Sound_paht_dir)
+Sound.remove('noise')
+Sound.remove('test')
+for k in Sound:
+    print(k)
 
-for i in range(0, len(file_list)):
-    OUTPUT_FILE = path_dir + "/" + str(file_list[i])
-    NOISE_FILE = "./n" + str(number) + ".wav"
-    ORIGINAL_FILE = path_dir + "/" + str(file_list[i])
+    Original_path_dir = './speech_commands_v0.02/speech_commands_v0.02/'+k+'/'
 
-    sound1 = AudioSegment.from_wav(NOISE_FILE)
-    sound2 = AudioSegment.from_wav(ORIGINAL_FILE)
+    Output_path_dir = './datas/'
 
-    combined_sounds = sound1 + sound1 + sound1 + sound1 + sound1 # 반복 횟수 조절
-    combined_sounds = combined_sounds - 30   # dB 조절 (+ $$$), (- $$$)
-    output = sound2.overlay(combined_sounds)
-    output = output.set_channels(MONO) # stereo to mono
-    output = output.set_frame_rate(SAMPLING_RATE) # frame_rate : 16000
+    file_list = os.listdir(Original_path_dir)
+    ##print(file_list)
+    file_string = '\n'.join(file_list)
+    ##print(file_string)
 
-    output.export(OUTPUT_FILE, format="wav")
 
-# check frame_rate
-from scipy.io.wavfile import read as read_wav
-import os
-os.chdir('./') # path
-sampling_rate, data = read_wav(OUTPUT_FILE)
-print("Frame_Rate : " + str(sampling_rate))
+    for i in range(0, int(len(file_list)/5)):
+        ORIGINAL_FILE = Original_path_dir + str(file_list[i])
+        NOISE_FILE = "./RestaurantNoise.wav"
+        OUTPUT_FILE = Output_path_dir + k + '/' + k + "_random_" + str(i)+".wav"
+        tTime = random.randrange(1, 20)
+        tTime = tTime/10
 
-# check channels
-import wave
-f1 = wave.open(OUTPUT_FILE, 'r')
-print("Channels : " + str(f1.getnchannels()))
+        sound1 = AudioSegment.from_wav(NOISE_FILE)
+        sound2 = AudioSegment.from_wav(ORIGINAL_FILE)
 
+        combined_sounds = sound1[SAMPLING_RATE*(tTime):SAMPLING_RATE*(tTime+1)]
+        combined_sounds = combined_sounds - tTime   # dB 조절 (+ $$$), (- $$$)
+        output = sound2.overlay(combined_sounds)
+        output = output.set_channels(MONO) # stereo to mono
+        output = output.set_frame_rate(SAMPLING_RATE) # frame_rate : 16000
+
+        output.export(OUTPUT_FILE, format="wav")
+
+    # check frame_rate
+    from scipy.io.wavfile import read as read_wav
+    import os
+    os.chdir('./') # path
+    sampling_rate, data = read_wav(OUTPUT_FILE)
+    print("Frame_Rate : " + str(sampling_rate))
+
+    # check channels
+    import wave
+    f1 = wave.open(OUTPUT_FILE, 'r')
+    print("Channels : " + str(f1.getnchannels()))
 
