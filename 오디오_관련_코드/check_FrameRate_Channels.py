56fb27172507985833615d916fd539a1fe190433
201524495 <49744580+201524495@users.noreply.github.com>
2021-08-20 23:49:54 +0900

    Update check_FrameRate_Channels.py

--- 오디오_관련_코드/check_FrameRate_Channels.py
+++ 오디오_관련_코드/check_FrameRate_Channels.py
@@ -1,13 +1,41 @@
-OUTPUT_FILE = "example1.wav" #file name
-
-# check frame_rate
 from scipy.io.wavfile import read as read_wav
 import os
-os.chdir('./') # path
-sampling_rate, data = read_wav(OUTPUT_FILE)
-print("Frame_Rate : " + str(sampling_rate))
-
-# check channels
 import wave
-f1 = wave.open(OUTPUT_FILE, 'r')
-print("Channels : " + str(f1.getnchannels()))
+import contextlib
+
+
+Sound_paht_dir = './datas'
+Sound = os.listdir(Sound_paht_dir)
+##Sound.remove('noise')
+Sound.remove('test')
+
+for k in Sound:
+    print(k)
+    Sound = k
+    path_dir = './datas/'+k+'/'
+    file_list = os.listdir(path_dir)
+    print(len(file_list))
+    file_string = '\n'.join(file_list)
+    ##print(file_string)
+
+    for i in range(0, len(file_list)):
+        # check frame_rate
+    ##    print( path_dir + str(i) )
+
+        os.chdir('./') # path
+        sampling_rate, data = read_wav(path_dir + file_list[i])
+        if sampling_rate < 16000:
+            print("File name : " + file_list[i] ,"Frame_Rate : " + str(sampling_rate), "Data : " + str(data))
+
+        with contextlib.closing(wave.open(path_dir + file_list[i], 'r')) as f:
+            frames = f.getnframes()
+            rate = f.getframerate()
+            length = frames / float(rate)
+
+            if length > 1:
+                print("over one sec : ",file_list[i] , length)
+
+    # check channels
+
+##    f1 = wave.open(path_dir + str(i), 'r')
+##    print("Channels : " + str(f1.getnchannels()))
