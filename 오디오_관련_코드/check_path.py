a86879f31eb1a9388eb1af678140fe3a242ef12f
201524495 <49744580+201524495@users.noreply.github.com>
2021-08-20 23:49:59 +0900

    Update check_path.py

--- 오디오_관련_코드/check_path.py
+++ 오디오_관련_코드/check_path.py
@@ -1,12 +1,14 @@
 import os
 
-path_dir = '../wav_file'
-
-file_list = os.listdir(path_dir)
-
-print(file_list)
-
+path_dir = './datas/'
+file_name = "yes"
+file_list = os.listdir(path_dir+file_name)
+print(file_list[3787])
 file_string = '\n'.join(file_list)
+print(file_string[-1])
 
-print(file_string)
-
+##i = 0
+##for filename in os.listdir(path_dir+file_name):
+####    print(filename)
+##    os.rename(path_dir+file_name+"/"+filename, path_dir+file_name+'/'+file_name+"_origin_"+str(i)+".wav")
+##    i += 1
