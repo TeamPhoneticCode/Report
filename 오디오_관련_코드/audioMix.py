895922a5756ae077caa3b08dcd1dff91c4f249c2
201524495 <49744580+201524495@users.noreply.github.com>
2021-08-20 23:48:43 +0900

    Update audioMix.py
    audio files mix in one folder

--- 오디오_관련_코드/audioMix.py
+++ 오디오_관련_코드/audioMix.py
@@ -1,29 +1,34 @@
 from pydub import AudioSegment
-
+import os
+import librosa
+import numpy as np
+import random
 ##import soundfile as sf
 ##from pydub.utils import mediainfo
 
 SAMPLING_RATE = 16000
 MONO = 1
 
-number = 45
-Sound = "left"
+Sound = "yes"
 
-for i in range(15, 27):
-    OUTPUT_FILE = "./mixedSound_"+str(number)+"_"+Sound+"_"+str(i)+".wav"
-    NOISE_FILE = "./n"+str(number)+".wav"
-    ORIGINAL_FILE = "./"+Sound+"_01.wav"
+OUTPUT_FILE = "./mixedSound_"+Sound+"_"+".wav"
+NOISE_FILE = "./RestaurantNoise.wav"
+ORIGINAL_FILE = "./"+Sound+"_0.wav"
 
-    sound1 = AudioSegment.from_wav(NOISE_FILE)
-    sound2 = AudioSegment.from_wav(ORIGINAL_FILE)
+sound1 = AudioSegment.from_wav(NOISE_FILE)
+sound2 = AudioSegment.from_wav(ORIGINAL_FILE)
+tTime = random.randrange(1, 20)
+tTime = tTime/10
+print(tTime)
+print(sound1)
+##combined_sounds = sound1 + sound1 + sound1 + sound1 + sound1 # 반복 횟수 조절
+##combined_sounds = combined_sounds + tTime   # dB 조절 (+ $$$), (- $$$)
+combined_sounds = sound1[SAMPLING_RATE*(tTime):SAMPLING_RATE*(tTime+1)]
+output = sound2.overlay(combined_sounds)
+output = output.set_channels(MONO) # stereo to mono
+output = output.set_frame_rate(SAMPLING_RATE) # frame_rate : 16000
 
-    combined_sounds = sound1 + sound1 + sound1 + sound1 + sound1 # 반복 횟수 조절
-    combined_sounds = combined_sounds - i   # dB 조절 (+ $$$), (- $$$)
-    output = sound2.overlay(combined_sounds)
-    output = output.set_channels(MONO) # stereo to mono
-    output = output.set_frame_rate(SAMPLING_RATE) # frame_rate : 16000
-
-    output.export(OUTPUT_FILE, format="wav")
+output.export(OUTPUT_FILE, format="wav")
 
 # check frame_rate
 from scipy.io.wavfile import read as read_wav
