004aa67daf3a7573af0d09cf0707f0dafe7d502c
201524495 <49744580+201524495@users.noreply.github.com>
2021-08-20 23:49:42 +0900

    Update change_FrameRate_Channels.py

--- 오디오_관련_코드/change_FrameRate_Channels.py
+++ 오디오_관련_코드/change_FrameRate_Channels.py
@@ -1,11 +1,29 @@
 from pydub import AudioSegment
+import os
+import librosa
+print(librosa.__version__)
+import numpy as np
 
 SAMPLING_RATE = 16000 # hz : 16000
 MONO = 1 # mono : 1 ,,,,stereo : 2
-OUTPUT_FILE = "example1.wav" #file name
+OUTPUT_FILE = 'RestaurantNoise.wav' #file name
+Sec = 3
 
-sound1 = AudioSegment.from_wav("./" + OUTPUT_FILE)
+path_dir = './'
+file_list = os.listdir(path_dir)
+for i in file_list:
+    if i == OUTPUT_FILE:
+        break
+print(i)
+
+sound1 = AudioSegment.from_wav(path_dir + OUTPUT_FILE)
+##sound1 = AudioSegment.from_mp3("./" + OUTPUT_FILE)
 sound1 = sound1.set_channels(MONO) # stereo to mono
 sound1 = sound1.set_frame_rate(SAMPLING_RATE) # frame_rate : 16000
 
 sound1.export(OUTPUT_FILE, format="wav")
+
+y, sr = librosa.load(path_dir + OUTPUT_FILE)
+print(y, sr)
+ny = y[:sr*Sec]
+librosa.output.write_wav("cut_" + OUTPUT_FILE, ny, sr)
